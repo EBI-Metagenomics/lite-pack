5d329e33d5e8e97413de1deb61e716a68c8de339
Danilo Horta <danilo.horta@pm.me>
2022-02-02 12:11:48 +0000

    lp_unpack_uint

--- src/lite_pack.h
+++ src/lite_pack.h
@@ -12,22 +12,22 @@ static inline int lip_format(uint8_t buf[static 1])
     return format_parse(buf[0]);
 }
 
-static inline void pack_true(uint8_t buf[static 1])
+static inline void lip_pack_true(uint8_t buf[static 1])
 {
     buf[0] = first_byte(FMT_TRUE);
 }
 
-static inline void pack_false(uint8_t buf[static 1])
+static inline void lip_pack_false(uint8_t buf[static 1])
 {
     buf[0] = first_byte(FMT_FALSE);
 }
 
-static inline void pack_bool(uint8_t buf[static 1], bool val)
+static inline void lip_pack_bool(uint8_t buf[static 1], bool val)
 {
     if (val)
-        pack_true(buf);
+        lip_pack_true(buf);
     else
-        pack_false(buf);
+        lip_pack_false(buf);
 }
 
 void pack_u8(uint8_t buf[static 1], unsigned val);
@@ -35,7 +35,7 @@ void pack_u16(uint8_t buf[static 1], unsigned val);
 void pack_u32(uint8_t buf[static 1], unsigned val);
 void pack_u64(uint8_t buf[static 1], unsigned long val);
 
-#define pack_uint(buf, val)                                                    \
+#define lip_pack_int(buf, val)                                                 \
     _Generic((val), uint8_t                                                    \
              : pack_u8(buf, val), uint16_t                                     \
              : pack_u16(buf, val), uint32_t                                    \
@@ -43,23 +43,48 @@ void pack_u64(uint8_t buf[static 1], unsigned long val);
              : pack_u64(buf, val), unsigned long                               \
              : pack_u64(buf, val))
 
-static inline void unpack_pfix_data(uint8_t buf[static 1]) {}
+static inline void unpack_fix_uint(uint8_t buf[static 1]) {}
 
-static inline void unpack_u8_data(uint8_t buf[static 1]) {}
+static inline void unpack_u8(uint8_t buf[static 1]) {}
 
-static inline void unpack_u16_data(uint8_t buf[static 1])
+static inline void unpack_u16(uint8_t buf[static 1])
 {
     *((uint16_t *)buf) = big_to_host_endianness(*((uint16_t *)buf));
 }
 
-static inline void unpack_u32_data(uint8_t buf[static 1])
+static inline void unpack_u32(uint8_t buf[static 1])
 {
     *((uint32_t *)buf) = big_to_host_endianness(*((uint32_t *)buf));
 }
 
-static inline void unpack_u64_data(uint8_t buf[static 1])
+static inline void unpack_u64(uint8_t buf[static 1])
 {
     *((uint64_t *)buf) = big_to_host_endianness(*((uint64_t *)buf));
+}
+
+static inline unsigned lip_unpack_uint(uint8_t buf[static 1])
+{
+    if (lip_format(buf) == FMT_POSITIVE_FIXINT)
+    {
+        unpack_fix_uint(buf);
+        return (uint8_t)buf[0];
+    }
+    else if (lip_format(buf) == FMT_UINT_8)
+    {
+        unpack_u8(buf);
+        return (uint8_t)buf[1];
+    }
+    else if (lip_format(buf) == FMT_UINT_16)
+    {
+        unpack_u16(buf + 1);
+        return *((uint16_t *)(buf + 1));
+    }
+    else if (lip_format(buf) == FMT_UINT_32)
+    {
+        unpack_u32(buf + 1);
+        return *((uint32_t *)(buf + 1));
+    }
+    BUG();
 }
 
 #endif
--- test/msgpack.c
+++ test/msgpack.c
@@ -9,19 +9,19 @@ static int test_pack_bool(void)
     uint8_t buf[1] = {0};
 
     clear(buf);
-    pack_bool(buf, false);
+    lip_pack_bool(buf, false);
     if (buf[0] != (uint8_t)0xc2) return 1;
 
     clear(buf);
-    pack_false(buf);
+    lip_pack_false(buf);
     if (buf[0] != (uint8_t)0xc2) return 1;
 
     clear(buf);
-    pack_bool(buf, true);
+    lip_pack_bool(buf, true);
     if (buf[0] != (uint8_t)0xc3) return 1;
 
     clear(buf);
-    pack_true(buf);
+    lip_pack_true(buf);
     if (buf[0] != (uint8_t)0xc3) return 1;
 
     return 0;
@@ -35,35 +35,27 @@ static int test_pack_unsigned(void)
     do
     {
         clear(buf);
-        pack_uint(buf, i);
+        lip_pack_int(buf, i);
 
         if (i <= 127U)
         {
             if (lip_format(buf) != FMT_POSITIVE_FIXINT) return 1;
-
-            unpack_pfix_data(buf);
-            if ((uint8_t)buf[0] != (uint8_t)i) return 1;
+            if (lip_unpack_uint(buf) != i) return 1;
         }
         else if (i <= 255U)
         {
             if (lip_format(buf) != FMT_UINT_8) return 1;
-
-            unpack_u8_data(buf);
-            if ((uint8_t)buf[1] != (uint8_t)i) return 1;
+            if (lip_unpack_uint(buf) != i) return 1;
         }
         else if (i <= 65535U)
         {
             if (lip_format(buf) != FMT_UINT_16) return 1;
-
-            unpack_u16_data(buf + 1);
-            if (*((uint16_t *)(buf + 1)) != (uint16_t)i) return 1;
+            if (lip_unpack_uint(buf) != i) return 1;
         }
         else if (i <= 4294967295U)
         {
             if (lip_format(buf) != FMT_UINT_32) return 1;
-
-            unpack_u32_data(buf + 1);
-            if (*((uint32_t *)(buf + 1)) != (uint32_t)i) return 1;
+            if (lip_unpack_uint(buf) != i) return 1;
         }
         else
             break;
@@ -81,7 +73,7 @@ static int test_pack_unsigned_long(void)
     for (unsigned long i = 0; i <= 127; ++i)
     {
         clear(buf);
-        pack_uint(buf, i);
+        lip_pack_int(buf, i);
 
         if (lip_format(buf) != FMT_POSITIVE_FIXINT) return 1;
 
@@ -92,7 +84,7 @@ static int test_pack_unsigned_long(void)
     for (unsigned long i = 128; i <= UINT8_MAX; ++i)
     {
         clear(buf);
-        pack_uint(buf, i);
+        lip_pack_int(buf, i);
 
         if (lip_format(buf) != FMT_UINT_8) return 1;
 
@@ -103,7 +95,7 @@ static int test_pack_unsigned_long(void)
     for (unsigned long i = UINT8_MAX + 1; i <= UINT16_MAX; ++i)
     {
         clear(buf);
-        pack_uint(buf, i);
+        lip_pack_int(buf, i);
 
         if (lip_format(buf) != FMT_UINT_16) return 1;
 
@@ -115,7 +107,7 @@ static int test_pack_unsigned_long(void)
          i += 256)
     {
         clear(buf);
-        pack_uint(buf, i);
+        lip_pack_int(buf, i);
 
         if (lip_format(buf) != FMT_UINT_32) return 1;
 
@@ -129,7 +121,7 @@ static int test_pack_unsigned_long(void)
     do
     {
         clear(buf);
-        pack_uint(buf, i);
+        lip_pack_int(buf, i);
 
         if (lip_format(buf) != FMT_UINT_64) return 1;
 
