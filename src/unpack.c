8b8b9f40bf455172b5f9126813f7339886c4e49d
Danilo Horta <danilo.horta@pm.me>
2022-02-08 21:43:15 +0000

    string working again

--- src/unpack.c
+++ src/unpack.c
@@ -192,13 +192,17 @@ unsigned lip_unpack_str_size(uint8_t const buf[static 1], unsigned *size)
     switch (lip_format(buf))
     {
     case LIP_FMT_FIXSTR:
-        *size = __lip_format_fix_pvalue(NUM8(buf[0]));
+        *size = (unsigned)__lip_format_fix_value(buf[0]);
+        return 1;
     case LIP_FMT_STR_8:
         *size = __lip_load_num8(buf + 1).u;
+        return 2;
     case LIP_FMT_STR_16:
         *size = __lip_load_num16(buf + 1).u;
+        return 3;
     case LIP_FMT_STR_32:
         *size = __lip_load_num32(buf + 1).u;
+        return 5;
     }
     return 0;
 }
--- test/string.c
+++ test/string.c
@@ -1,6 +1,16 @@
 #include "helper.h"
 #include "lite_pack.h"
 
+static unsigned const sizes[] = {0, 0x1F, 0x20, 0xFF, 0x100, 0xFFFF, 0x10000U};
+static unsigned const pack_sizes[] = {
+    1, 1, 2, 2, 3, 3, 5,
+};
+
+static int const formats[] = {
+    LIP_FMT_FIXSTR, LIP_FMT_FIXSTR, LIP_FMT_STR_8,  LIP_FMT_STR_8,
+    LIP_FMT_STR_16, LIP_FMT_STR_16, LIP_FMT_STR_32,
+};
+
 int main(void)
 {
     unsigned long buf_size = 0x10000UL + 6UL;
@@ -11,29 +21,22 @@ int main(void)
     char *out = malloc(out_size);
     if (!out) exit(1);
 
-    unsigned lengths[] = {0, 0x1F, 0x20, 0xFF, 0x100, 0xFFFF, 0x10000U};
-    int formats[] = {
-        LIP_FMT_FIXSTR, LIP_FMT_FIXSTR, LIP_FMT_STR_8,  LIP_FMT_STR_8,
-        LIP_FMT_STR_16, LIP_FMT_STR_16, LIP_FMT_STR_32,
-    };
-    unsigned sizes[] = {
-        1, 1, 2, 2, 3, 3, 5,
-    };
-
     char const *str = 0;
 
-    for (unsigned i = 0; i < array_size(lengths); ++i)
+    for (unsigned i = 0; i < array_size(sizes); ++i)
     {
         memset(buf, 0, buf_size);
 
-        str = lorem_new(lengths[i]);
+        str = lorem_new(sizes[i]);
         unsigned len = (unsigned)strlen(str);
-        if (lip_pack_str_size(buf, len) != sizes[i]) ERROR;
-        if (lip_pack_str_data(buf + sizes[i], len, str) != lengths[i]) ERROR;
+        if (lip_pack_str_size(buf, len) != pack_sizes[i]) ERROR;
+        if (lip_pack_str_data(buf + pack_sizes[i], len, str) != sizes[i]) ERROR;
 
         if (lip_format(buf) != formats[i]) ERROR;
-        if (lip_unpack_str_size(buf, &len) != sizes[i]) ERROR;
-        // if (strcmp(lip_unpack_str(buf, out), str)) goto error;
+        if (lip_unpack_str_size(buf, &len) != pack_sizes[i]) ERROR;
+        if (len != sizes[i]) ERROR;
+        if (lip_unpack_str_data(buf + pack_sizes[i], len, out) != sizes[i]) ERROR;
+        if (strncmp(out, str, len) != 0) ERROR;
 
         free((void *)str);
     }
