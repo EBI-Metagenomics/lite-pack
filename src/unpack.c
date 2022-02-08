80328445ce1b4174e489b89e4897c4ab8784054f
Danilo Horta <danilo.horta@pm.me>
2022-02-08 22:07:20 +0000

    avoid more undefined behavior

--- src/unpack.c
+++ src/unpack.c
@@ -1,7 +1,6 @@
 #include "unpack.h"
 #include "lite_pack.h"
 #include "load.h"
-#include "load_body.h"
 
 unsigned __lip_unpack_u8(uint8_t const buf[static 1], uint8_t *val)
 {
@@ -9,10 +8,12 @@ unsigned __lip_unpack_u8(uint8_t const buf[static 1], uint8_t *val)
     {
     case LIP_FMT_POSITIVE_FIXINT:
     case LIP_FMT_NEGATIVE_FIXINT:
-        return __lip_load_num8_body(buf, (union num8 *)val);
+        *val = __lip_load_num8(buf).u;
+        return 1;
     case LIP_FMT_UINT_8:
     case LIP_FMT_INT_8:
-        return 1 + __lip_load_num8_body(buf + 1, (union num8 *)val);
+        *val = __lip_load_num8(buf + 1).u;
+        return 2;
     }
     return 0;
 }
@@ -23,13 +24,16 @@ unsigned __lip_unpack_u16(uint8_t const buf[static 1], uint16_t *val)
     {
     case LIP_FMT_POSITIVE_FIXINT:
     case LIP_FMT_NEGATIVE_FIXINT:
-        return __lip_load_num8_body(buf, (union num8 *)val);
+        *val = __lip_load_num8(buf).u;
+        return 1;
     case LIP_FMT_UINT_8:
     case LIP_FMT_INT_8:
-        return 1 + __lip_load_num8_body(buf + 1, (union num8 *)val);
+        *val = __lip_load_num8(buf + 1).u;
+        return 2;
     case LIP_FMT_UINT_16:
     case LIP_FMT_INT_16:
-        return 1 + __lip_load_num16_body(buf + 1, (union num16 *)val);
+        *val = __lip_load_num16(buf + 1).u;
+        return 3;
     }
     return 0;
 }
@@ -40,16 +44,20 @@ unsigned __lip_unpack_u32(uint8_t const buf[static 1], uint32_t *val)
     {
     case LIP_FMT_POSITIVE_FIXINT:
     case LIP_FMT_NEGATIVE_FIXINT:
-        return __lip_load_num8_body(buf, (union num8 *)val);
+        *val = __lip_load_num8(buf).u;
+        return 1;
     case LIP_FMT_UINT_8:
     case LIP_FMT_INT_8:
-        return 1 + __lip_load_num8_body(buf + 1, (union num8 *)val);
+        *val = __lip_load_num8(buf + 1).u;
+        return 2;
     case LIP_FMT_UINT_16:
     case LIP_FMT_INT_16:
-        return 1 + __lip_load_num16_body(buf + 1, (union num16 *)val);
+        *val = __lip_load_num16(buf + 1).u;
+        return 3;
     case LIP_FMT_UINT_32:
     case LIP_FMT_INT_32:
-        return 1 + __lip_load_num32_body(buf + 1, (union num32 *)val);
+        *val = __lip_load_num32(buf + 1).u;
+        return 5;
     }
     return 0;
 }
@@ -60,19 +68,24 @@ unsigned __lip_unpack_u64(uint8_t const buf[static 1], uint64_t *val)
     {
     case LIP_FMT_POSITIVE_FIXINT:
     case LIP_FMT_NEGATIVE_FIXINT:
-        return __lip_load_num8_body(buf, (union num8 *)val);
+        *val = __lip_load_num8(buf).u;
+        return 1;
     case LIP_FMT_UINT_8:
     case LIP_FMT_INT_8:
-        return 1 + __lip_load_num8_body(buf + 1, (union num8 *)val);
+        *val = __lip_load_num8(buf + 1).u;
+        return 2;
     case LIP_FMT_UINT_16:
     case LIP_FMT_INT_16:
-        return 1 + __lip_load_num16_body(buf + 1, (union num16 *)val);
+        *val = __lip_load_num16(buf + 1).u;
+        return 3;
     case LIP_FMT_UINT_32:
     case LIP_FMT_INT_32:
-        return 1 + __lip_load_num32_body(buf + 1, (union num32 *)val);
+        *val = __lip_load_num32(buf + 1).u;
+        return 5;
     case LIP_FMT_UINT_64:
     case LIP_FMT_INT_64:
-        return 1 + __lip_load_num64_body(buf + 1, (union num64 *)val);
+        *val = __lip_load_num64(buf + 1).u;
+        return 9;
     }
     return 0;
 }
@@ -83,10 +96,12 @@ unsigned __lip_unpack_i8(uint8_t const buf[static 1], int8_t *val)
     {
     case LIP_FMT_POSITIVE_FIXINT:
     case LIP_FMT_NEGATIVE_FIXINT:
-        return __lip_load_num8_body(buf, (union num8 *)val);
+        *val = __lip_load_num8(buf).i;
+        return 1;
     case LIP_FMT_UINT_8:
     case LIP_FMT_INT_8:
-        return 1 + __lip_load_num8_body(buf + 1, (union num8 *)val);
+        *val = __lip_load_num8(buf + 1).i;
+        return 2;
     }
     return 0;
 }
@@ -97,13 +112,16 @@ unsigned __lip_unpack_i16(uint8_t const buf[static 1], int16_t *val)
     {
     case LIP_FMT_POSITIVE_FIXINT:
     case LIP_FMT_NEGATIVE_FIXINT:
-        return __lip_load_num8_body(buf, (union num8 *)val);
+        *val = __lip_load_num8(buf).i;
+        return 1;
     case LIP_FMT_UINT_8:
     case LIP_FMT_INT_8:
-        return 1 + __lip_load_num8_body(buf + 1, (union num8 *)val);
+        *val = __lip_load_num8(buf + 1).i;
+        return 2;
     case LIP_FMT_UINT_16:
     case LIP_FMT_INT_16:
-        return 1 + __lip_load_num16_body(buf + 1, (union num16 *)val);
+        *val = __lip_load_num16(buf + 1).i;
+        return 3;
     }
     return 0;
 }
@@ -114,16 +132,20 @@ unsigned __lip_unpack_i32(uint8_t const buf[static 1], int32_t *val)
     {
     case LIP_FMT_POSITIVE_FIXINT:
     case LIP_FMT_NEGATIVE_FIXINT:
-        return __lip_load_num8_body(buf, (union num8 *)val);
+        *val = __lip_load_num8(buf).i;
+        return 1;
     case LIP_FMT_UINT_8:
     case LIP_FMT_INT_8:
-        return 1 + __lip_load_num8_body(buf + 1, (union num8 *)val);
+        *val = __lip_load_num8(buf + 1).i;
+        return 2;
     case LIP_FMT_UINT_16:
     case LIP_FMT_INT_16:
-        return 1 + __lip_load_num16_body(buf + 1, (union num16 *)val);
+        *val = __lip_load_num16(buf + 1).i;
+        return 3;
     case LIP_FMT_UINT_32:
     case LIP_FMT_INT_32:
-        return 1 + __lip_load_num32_body(buf + 1, (union num32 *)val);
+        *val = __lip_load_num32(buf + 1).i;
+        return 5;
     }
     return 0;
 }
@@ -134,19 +156,24 @@ unsigned __lip_unpack_i64(uint8_t const buf[static 1], int64_t *val)
     {
     case LIP_FMT_POSITIVE_FIXINT:
     case LIP_FMT_NEGATIVE_FIXINT:
-        return __lip_load_num8_body(buf, (union num8 *)val);
+        *val = __lip_load_num8(buf).i;
+        return 1;
     case LIP_FMT_UINT_8:
     case LIP_FMT_INT_8:
-        return 1 + __lip_load_num8_body(buf + 1, (union num8 *)val);
+        *val = __lip_load_num8(buf + 1).i;
+        return 2;
     case LIP_FMT_UINT_16:
     case LIP_FMT_INT_16:
-        return 1 + __lip_load_num16_body(buf + 1, (union num16 *)val);
+        *val = __lip_load_num16(buf + 1).i;
+        return 3;
     case LIP_FMT_UINT_32:
     case LIP_FMT_INT_32:
-        return 1 + __lip_load_num32_body(buf + 1, (union num32 *)val);
+        *val = __lip_load_num32(buf + 1).i;
+        return 5;
     case LIP_FMT_UINT_64:
     case LIP_FMT_INT_64:
-        return 1 + __lip_load_num64_body(buf + 1, (union num64 *)val);
+        *val = __lip_load_num64(buf + 1).i;
+        return 9;
     }
     return 0;
 }
@@ -154,38 +181,29 @@ unsigned __lip_unpack_i64(uint8_t const buf[static 1], int64_t *val)
 unsigned __lip_unpack_f32(uint8_t const buf[static 5], float *val)
 {
     if (lip_format(buf) == LIP_FMT_FLOAT_32)
-        return 1 + __lip_load_num32_body(buf + 1, (union num32 *)val);
+    {
+        *val = __lip_load_num32(buf + 1).f;
+        return 5;
+    }
     return 0;
 }
 
 unsigned __lip_unpack_f64(uint8_t const buf[static 5], double *val)
 {
-    if (lip_format(buf) == LIP_FMT_FLOAT_64)
-        return 1 + __lip_load_num64_body(buf + 1, (union num64 *)val);
-
     if (lip_format(buf) == LIP_FMT_FLOAT_32)
-        return 1 + __lip_load_num32_body(buf + 1, (union num32 *)val);
-
-    return 0;
-}
-
-#if 0
-char *lip_unpack_str(uint8_t const buf[static 1], char str[static 1])
-{
-    switch (lip_format(buf))
     {
-    case LIP_FMT_FIXSTR:
-        return __lip_load_fix_str(buf, str);
-    case LIP_FMT_STR_8:
-        return __lip_load_str8(buf, str);
-    case LIP_FMT_STR_16:
-        return __lip_load_str16(buf, str);
-    case LIP_FMT_STR_32:
-        return __lip_load_str32(buf, str);
+        *val = __lip_load_num32(buf + 1).f;
+        return 5;
     }
+
+    if (lip_format(buf) == LIP_FMT_FLOAT_64)
+    {
+        *val = __lip_load_num64(buf + 1).d;
+        return 9;
+    }
+
     return 0;
 }
-#endif
 
 unsigned lip_unpack_str_size(uint8_t const buf[static 1], unsigned *size)
 {
@@ -210,7 +228,8 @@ unsigned lip_unpack_str_size(uint8_t const buf[static 1], unsigned *size)
 unsigned lip_unpack_str_data(uint8_t const buf[static 1], unsigned size,
                              char str[static 1])
 {
-    return __lip_load_str_body(buf, size, str);
+    memcpy(str, buf, size);
+    return size;
 }
 
 unsigned lip_unpack_array_size(uint8_t const buf[static 1], unsigned *size)
