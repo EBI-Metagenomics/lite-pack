aea1050a3be74dff559dafe1bc48c7d88c730fb9
Danilo Horta <danilo.horta@pm.me>
2022-02-09 10:48:57 +0000

    unsigned

--- src/store_float.h
+++ src/store_float.h
@@ -4,13 +4,13 @@
 #include "first_byte.h"
 #include "store_number.h"
 
-static inline unsigned long __lip_store_f32(uint8_t buf[static 5], float val)
+static inline unsigned __lip_store_f32(uint8_t buf[static 5], float val)
 {
     buf[0] = __lip_first_byte(LIP_FMT_FLOAT_32);
     return 1 + lip_store_num32(buf + 1, __LIP_NUM32(val));
 }
 
-static inline unsigned long __lip_store_f64(uint8_t buf[static 9], double val)
+static inline unsigned __lip_store_f64(uint8_t buf[static 9], double val)
 {
     buf[0] = __lip_first_byte(LIP_FMT_FLOAT_64);
     return 1 + lip_store_num64(buf + 1, __LIP_NUM64(val));
