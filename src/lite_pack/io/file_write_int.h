11f8cd7994cd1ff4ce679825e5bf0b848c1d1337
Danilo Horta <danilo.horta@pm.me>
2022-02-14 15:12:06 +0000

    io_file test

--- src/lite_pack/io/file_write_int.h
+++ src/lite_pack/io/file_write_int.h
@@ -2,33 +2,40 @@
 #define LIP_IO_FILE_WRITE_INT_H
 
 #include "lite_pack/export.h"
+#include <stdint.h>
 
-#define __lip_write_signed(ctx, val)                                           \
-    sizeof(val) == 1   ? __lip_write_i8(ctx, (int8_t)(val))                    \
-    : sizeof(val) == 2 ? __lip_write_i16(ctx, (int16_t)(val))                  \
-    : sizeof(val) == 4 ? __lip_write_i32(ctx, (int32_t)(val))                  \
-    : sizeof(val) == 8 ? __lip_write_i64(ctx, (int64_t)(val))                  \
-                       : 0
+#define __lip_write_signed(ctx, v)                                             \
+    sizeof(v) == 1   ? __lip_write_i8(ctx, (int8_t)(v))                        \
+    : sizeof(v) == 2 ? __lip_write_i16(ctx, (int16_t)(v))                      \
+    : sizeof(v) == 4 ? __lip_write_i32(ctx, (int32_t)(v))                      \
+    : sizeof(v) == 8 ? __lip_write_i64(ctx, (int64_t)(v))                      \
+                     : 0
 
-#define __lip_write_unsigned(ctx, val)                                         \
-    sizeof(val) == 1   ? __lip_write_u8(ctx, (uint8_t)(val))                   \
-    : sizeof(val) == 2 ? __lip_write_u16(ctx, (uint16_t)(val))                 \
-    : sizeof(val) == 4 ? __lip_write_u32(ctx, (uint32_t)(val))                 \
-    : sizeof(val) == 8 ? __lip_write_u64(ctx, (uint64_t)(val))                 \
-                       : 0U
+#define __lip_write_unsigned(ctx, v)                                           \
+    sizeof(v) == 1   ? __lip_write_u8(ctx, (uint8_t)(v))                       \
+    : sizeof(v) == 2 ? __lip_write_u16(ctx, (uint16_t)(v))                     \
+    : sizeof(v) == 4 ? __lip_write_u32(ctx, (uint32_t)(v))                     \
+    : sizeof(v) == 8 ? __lip_write_u64(ctx, (uint64_t)(v))                     \
+                     : 0U
+
+#define __lip_write_int_typed(ctx, v)                                          \
+    _Generic((v), signed char                                                  \
+             : __lip_write_signed(ctx, v), signed short                        \
+             : __lip_write_signed(ctx, v), signed int                          \
+             : __lip_write_signed(ctx, v), signed long                         \
+             : __lip_write_signed(ctx, v), signed long long                    \
+             : __lip_write_signed(ctx, v), unsigned char                       \
+             : __lip_write_unsigned(ctx, v), unsigned short                    \
+             : __lip_write_unsigned(ctx, v), unsigned int                      \
+             : __lip_write_unsigned(ctx, v), unsigned long                     \
+             : __lip_write_unsigned(ctx, v), unsigned long long                \
+             : __lip_write_unsigned(ctx, v))
 
 #define __lip_write_int(ctx, val)                                              \
-    _Generic((val), signed char                                                \
-             : __lip_write_signed(ctx, val), signed short                      \
-             : __lip_write_signed(ctx, val), signed int                        \
-             : __lip_write_signed(ctx, val), signed long                       \
-             : __lip_write_signed(ctx, val), signed long long                  \
-             : __lip_write_signed(ctx, val), unsigned char                     \
-             : __lip_write_unsigned(ctx, val), unsigned short                  \
-             : __lip_write_unsigned(ctx, val), unsigned int                    \
-             : __lip_write_unsigned(ctx, val), unsigned long                   \
-             : __lip_write_unsigned(ctx, val), unsigned long long              \
-             : __lip_write_unsigned(ctx, val))
+    ({                                                                         \
+        typeof(val) tmp = (val);                                               \
+        __lip_write_int_typed(ctx, __builtin_constant_p(val) ? tmp : val);     \
+    })
 
 struct lip_ctx_file;
 
---
+++ test/io_file.c
@@ -0,0 +1,61 @@
+#include "helper.h"
+#include "lite_pack/io/file.h"
+#include "lite_pack/lite_pack.h"
+
+static void test_correct_usage()
+{
+    struct lip_ctx_file ctx = {0};
+
+    ctx.fp = fopen(TMPDIR "/io_file.mp", "wb");
+    lip_write_int(&ctx, 0);
+    lip_write_int(&ctx, 39290);
+    lip_write_array_size(&ctx, 2);
+    lip_write_float(&ctx, 3.2f);
+    lip_write_str_size(&ctx, 5);
+    lip_write_str_data(&ctx, 5, "hello");
+    if (ctx.error) ERROR;
+    fclose(ctx.fp);
+
+    ctx.fp = fopen(TMPDIR "/io_file.mp", "rb");
+    int ival = 0;
+    lip_read_int(&ctx, &ival);
+    if (ival != 0) ERROR;
+    lip_read_int(&ctx, &ival);
+    if (ival != 39290) ERROR;
+    unsigned uval = 0;
+    lip_read_array_size(&ctx, &uval);
+    if (uval != 2) ERROR;
+    float fval = 0.f;
+    lip_read_float(&ctx, &fval);
+    if (fval != 3.2f) ERROR;
+    lip_read_str_size(&ctx, &uval);
+    if (uval != 5) ERROR;
+    char hello[6] = {0};
+    lip_read_str_data(&ctx, 5, hello);
+    if (strncmp(hello, "hello", 5) != 0) ERROR;
+    if (ctx.error) ERROR;
+    fclose(ctx.fp);
+}
+
+static void test_incorrect_usage()
+{
+    struct lip_ctx_file ctx = {0};
+
+    ctx.fp = fopen(TMPDIR "/io_file.mp", "wb");
+    lip_write_int(&ctx, INT64_MAX);
+    if (ctx.error) ERROR;
+    fclose(ctx.fp);
+
+    ctx.fp = fopen(TMPDIR "/io_file.mp", "rb");
+    uint16_t x = 0;
+    lip_read_int(&ctx, &x);
+    if (!ctx.error) ERROR;
+    fclose(ctx.fp);
+}
+
+int main(void)
+{
+    test_correct_usage();
+    test_incorrect_usage();
+    return 0;
+}
