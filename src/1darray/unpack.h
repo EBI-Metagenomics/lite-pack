7ad525c2266e72ca3b7f689dc9b2de3051bda945
Danilo Horta <danilo.horta@pm.me>
2022-02-28 13:14:22 +0000

    fix unpack 1darray

--- src/1darray/unpack.h
+++ src/1darray/unpack.h
@@ -12,8 +12,8 @@ static inline unsigned unpack_1darray_size_type(unsigned char buf[],
     uint8_t typ = 0;
     unsigned sz = lip_unpack_ext_size_type(buf, size, &typ);
     if (typ > LIP_1DARRAY_F64) return 0;
-    if (*size % _1darray_item_size(*type)) return 0;
     *type = typ;
+    if (*size % _1darray_item_size(*type)) return 0;
     *size /= _1darray_item_size(*type);
     return sz;
 }
