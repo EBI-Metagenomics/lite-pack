483fb82bf569f7e49e31fa7e02b9bcb4f7e0e996
Danilo Horta <danilo.horta@pm.me>
2022-04-24 23:12:49 +0100

    fix warnings

--- test/file.c
+++ test/file.c
@@ -7,31 +7,31 @@ static void test_correct_usage()
     struct lip_file file = {0};
 
     file.fp = fopen(TMPDIR "/file.mp", "wb");
-    lip_write_int(&file, 0);
-    lip_write_int(&file, 39290);
-    lip_write_array_size(&file, 2);
-    lip_write_float(&file, 3.2f);
-    lip_write_str_size(&file, 5);
-    lip_write_str_data(&file, 5, "hello");
+    if (!lip_write_int(&file, 0)) ERROR;
+    if (!lip_write_int(&file, 39290)) ERROR;
+    if (!lip_write_array_size(&file, 2)) ERROR;
+    if (!lip_write_float(&file, 3.2f)) ERROR;
+    if (!lip_write_str_size(&file, 5)) ERROR;
+    if (!lip_write_str_data(&file, 5, "hello")) ERROR;
     if (file.error) ERROR;
     fclose(file.fp);
 
     file.fp = fopen(TMPDIR "/file.mp", "rb");
     int ival = 0;
-    lip_read_int(&file, &ival);
+    if (!lip_read_int(&file, &ival)) ERROR;
     if (ival != 0) ERROR;
-    lip_read_int(&file, &ival);
+    if (!lip_read_int(&file, &ival)) ERROR;
     if (ival != 39290) ERROR;
     unsigned uval = 0;
-    lip_read_array_size(&file, &uval);
+    if (!lip_read_array_size(&file, &uval)) ERROR;
     if (uval != 2) ERROR;
     float fval = 0.f;
-    lip_read_float(&file, &fval);
+    if (!lip_read_float(&file, &fval)) ERROR;
     if (fval != 3.2f) ERROR;
-    lip_read_str_size(&file, &uval);
+    if (!lip_read_str_size(&file, &uval)) ERROR;
     if (uval != 5) ERROR;
     char hello[6] = {0};
-    lip_read_str_data(&file, 5, hello);
+    if (!lip_read_str_data(&file, 5, hello)) ERROR;
     if (strncmp(hello, "hello", 5) != 0) ERROR;
     if (file.error) ERROR;
     fclose(file.fp);
@@ -42,13 +42,13 @@ static void test_incorrect_usage()
     struct lip_file file = {0};
 
     file.fp = fopen(TMPDIR "/file.mp", "wb");
-    lip_write_int(&file, INT64_MAX);
+    if (!lip_write_int(&file, INT64_MAX)) ERROR;
     if (file.error) ERROR;
     fclose(file.fp);
 
     file.fp = fopen(TMPDIR "/file.mp", "rb");
     uint16_t x = 0;
-    lip_read_int(&file, &x);
+    if (lip_read_int(&file, &x)) ERROR;
     if (!file.error) ERROR;
     fclose(file.fp);
 }
