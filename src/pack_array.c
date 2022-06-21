a5a1a64b363a99a4e8d2c7c3a1e397b71ec25313
Danilo Horta <danilo.horta@pm.me>
2022-06-21 22:33:03 +0100

    Hide lip_store_arrays

--- src/pack_array.c
+++ src/pack_array.c
@@ -1,5 +1,24 @@
 #include "lite_pack/pack_array.h"
-#include "lite_pack/store_array.h"
+#include "lite_pack/first_byte.h"
+#include "lite_pack/store_number.h"
+
+static inline unsigned __lip_store_fixarray(unsigned char buf[], uint8_t size)
+{
+    buf[0] = __lip_first_byte_fix(LIP_FMT_FIXARRAY, size);
+    return 1;
+}
+
+static inline unsigned __lip_store_array16(unsigned char buf[], uint16_t size)
+{
+    buf[0] = __lip_first_byte(LIP_FMT_ARRAY_16);
+    return __lip_store_num16(buf + 1, __LIP_NUM16(size)) + 1;
+}
+
+static inline unsigned __lip_store_array32(unsigned char buf[], uint32_t size)
+{
+    buf[0] = __lip_first_byte(LIP_FMT_ARRAY_32);
+    return __lip_store_num32(buf + 1, __LIP_NUM32(size)) + 1;
+}
 
 unsigned lip_pack_array_size(unsigned char buf[], unsigned size)
 {
