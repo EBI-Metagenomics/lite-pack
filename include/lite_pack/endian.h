35e1d1c77fcc0947d7dea9ea90a595dcc2c42468
Danilo Horta <danilo.horta@pm.me>
2022-11-18 21:38:26 +0000

    dont expose non-standard libs

--- include/lite_pack/endian.h
+++ include/lite_pack/endian.h
@@ -1,50 +1,17 @@
 #ifndef LITE_PACK_ENDIAN_H
 #define LITE_PACK_ENDIAN_H
 
-#include "lite_pack/compiler.h"
-#include <arpa/inet.h>
+#include "lite_pack/export.h"
 #include <stdbool.h>
+#include <stdint.h>
 
-#if __APPLE__
-#include <machine/endian.h>
-#else
-#include <endian.h>
-#endif
+LIP_API uint16_t __lip_htons(uint16_t x);
+LIP_API uint32_t __lip_htonl(uint32_t x);
+LIP_API uint64_t __lip_htonll(uint64_t x);
 
-#if BYTE_ORDER != LITTLE_ENDIAN && BYTE_ORDER != BIG_ENDIAN
-#error We are supporting little and big endian only for now.
-#endif
-
-#ifndef htonll
-
-#ifndef BYTE_ORDER
-#define BYTE_ORDER __BYTE_ORDER__
-#endif
-
-#ifndef BIG_ENDIAN
-#define BIG_ENDIAN __BIG_ENDIAN__
-#endif
-
-#if BYTE_ORDER == BIG_ENDIAN
-
-static inline uint64_t htonll(uint64_t x) { return x; }
-static inline uint64_t ntohll(uint64_t x) { return x; }
-
-#else
-
-static inline uint64_t htonll(uint64_t x) { return __builtin_bswap64(x); }
-static inline uint64_t ntohll(uint64_t x) { return __builtin_bswap64(x); }
-
-#endif
-#endif
-
-static inline uint16_t __lip_htons(uint16_t x) { return htons(x); }
-static inline uint32_t __lip_htonl(uint32_t x) { return htonl(x); }
-static inline uint64_t __lip_htonll(uint64_t x) { return htonll(x); }
-
-static inline uint16_t __lip_ntohs(uint16_t x) { return ntohs(x); }
-static inline uint32_t __lip_ntohl(uint32_t x) { return ntohl(x); }
-static inline uint64_t __lip_ntohll(uint64_t x) { return ntohll(x); }
+LIP_API uint16_t __lip_ntohs(uint16_t x);
+LIP_API uint32_t __lip_ntohl(uint32_t x);
+LIP_API uint64_t __lip_ntohll(uint64_t x);
 
 #define __lip_big_endian(x)                                                    \
     _Generic((x), uint16_t                                                     \
@@ -64,13 +31,6 @@ static inline uint64_t __lip_ntohll(uint64_t x) { return ntohll(x); }
              : __lip_ntohl, int64_t                                            \
              : __lip_ntohll)(x)
 
-static inline bool __lip_is_big_endian(void)
-{
-#if BYTE_ORDER == BIG_ENDIAN
-    return true;
-#else
-    return false;
-#endif
-}
+LIP_API bool __lip_is_big_endian(void);
 
 #endif
